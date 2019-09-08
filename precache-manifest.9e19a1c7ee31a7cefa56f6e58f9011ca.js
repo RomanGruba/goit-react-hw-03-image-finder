af9dfa2fc6abea57166895a26f68a8c7e9e8f5e8
Roman Gruba <roman.gruba@gmail.com>
2019-09-08 10:51:56 +0300

    Updates

---
+++ precache-manifest.9e19a1c7ee31a7cefa56f6e58f9011ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4baa85f227ffcbeed7c09a19344006",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "c88fb1156f8bb475b147",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.b374fb8c.chunk.css"
+  },
+  {
+    "revision": "1169a6247013fdf4ed62",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.51fb422d.chunk.css"
+  },
+  {
+    "revision": "c88fb1156f8bb475b147",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.c536f604.chunk.js"
+  },
+  {
+    "revision": "1169a6247013fdf4ed62",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.a4338a45.chunk.js"
+  },
+  {
+    "revision": "aa3031e0a5d0d5cc1954",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.c9496cc6.js"
+  }
+]);
